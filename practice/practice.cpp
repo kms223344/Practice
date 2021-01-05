a36e66cc7b68db4d4f017246f68e73eb24404792
Astilate <pikk5500.l@gmail.com>
2021-01-05 10:55:59 +0900

    시간 추가

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,18 +1,30 @@
 ﻿#include<stdio.h>
 #include<conio.h>
+#include<time.h>
+enum keyinput {
+	L1 = 'z',
+	L2 = 'x',
+	R1 = '/',
+	R2 = '.'
+};
 int main()
 {
+	clock_t a, b;
 	while (true)
 	{
-		char c = _getche();
+		a = clock();
+		char c = _getch();
+		b = clock();
 		switch (c)
 		{
-		case 'z': printf("L1"); break;
-		case 'x': printf("L2"); break;
-		case '.': printf("R2"); break;
-		case '/': printf("R1"); break;
+		case L1: printf("L1"); break;
+		case L2: printf("L2"); break;
+		case R2: printf("R2"); break;
+		case R1: printf("R1"); break;
 		case 'q': return 0;
 		}
-		printf("\r");
+		double result = (double)(b - a);
+		printf("\t%gms\n", result);
+		//printf("\r");
 	}
 }
