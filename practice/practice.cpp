778f545fcb1c81fc941ba00cab9aa2ad94f40c4d
Astilate <pikk5500.l@gmail.com>
2021-01-05 10:43:41 +0900

    키보드 입력

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,5 +1,18 @@
 ﻿#include<stdio.h>
+#include<conio.h>
 int main()
 {
-	printf("Hello world!");
+	while (true)
+	{
+		char c = _getche();
+		switch (c)
+		{
+		case 'z': printf("L1"); break;
+		case 'x': printf("L2"); break;
+		case '.': printf("R2"); break;
+		case '/': printf("R1"); break;
+		case 'q': return 0;
+		}
+		printf("\r");
+	}
 }
